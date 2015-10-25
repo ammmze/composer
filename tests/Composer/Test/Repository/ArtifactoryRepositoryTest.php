f928d2ae5a8009ebfb9d8ef53fbf5f3c7a05fa07
Patrick Zhang <pzhang@parchment.com>
2015-10-25 15:35:37 -0400

    Fix the unit test to make it work with caching

--- tests/Composer/Test/Repository/ArtifactoryRepositoryTest.php
+++ tests/Composer/Test/Repository/ArtifactoryRepositoryTest.php
@@ -45,7 +45,7 @@ class ArtifactoryRepositoryTest extends TestCase
             ->method('searchForArtifacts')
             ->willReturn('{"results":[{"uri":"http://localhost/api/storage/libs-snapshot-local/vendor0/0.0.1/package0-0.0.1.zip"},{"uri":"http://localhost/api/storage/libs-snapshot-local/composer/1.0.0-alpha6/composer-1.0.0-alpha6.zip"}]}');
 
-        $repo->expects($this->exactly(2))
+        $repo->expects($this->atMost(2))
             ->method('getArtifactData')
             ->withConsecutive(
                 array('http://localhost/api/storage/libs-snapshot-local/vendor0/0.0.1/package0-0.0.1.zip'),
@@ -56,7 +56,7 @@ class ArtifactoryRepositoryTest extends TestCase
                 '{"metadataUri":"http://localhost/api/storage/libs-snapshot-local/composer/1.0.0-alpha6/composer-1.0.0-alpha6.zip?mdns","repo":"libs-snapshot-local","path":"/composer/1.0.0-alpha6/composer-1.0.0-alpha6.zip","created":"2015-07-06T14:03:30.779-07:00","createdBy":"admin","lastModified":"2015-07-06T14:03:30.779-07:00","modifiedBy":"admin","lastUpdated":"2015-07-06T14:03:30.781-07:00","downloadUri":"http://localhost/libs-snapshot-local/composer/1.0.0-alpha6/composer-1.0.0-alpha6.zip","mimeType":"application/zip","size":22194,"checksums":{"sha1":"9d7e1e86f7a0bcdd96a20e0f30c2fc63c0b8f5c6","md5":"f8c749286816f1d87a8902f1adab6203"},"originalChecksums":{"sha1":"9d7e1e86f7a0bcdd96a20e0f30c2fc63c0b8f5c6","md5":"f8c749286816f1d87a8902f1adab6203"},"uri":"http://localhost/api/storage/libs-snapshot-local/composer/1.0.0-alpha6/composer-1.0.0-alpha6.zip"}'
             );
 
-        $repo->expects($this->exactly(2))
+        $repo->expects($this->atMost(2))
             ->method('getComposer')
             ->withConsecutive(
                 array('http://localhost/libs-snapshot-local/vendor0/0.0.1/package0-0.0.1.zip'),
